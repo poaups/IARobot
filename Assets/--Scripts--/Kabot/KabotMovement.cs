49429ec9fe2be5819c457172d2908b4cbde82d24
1Coquo <1coquo@gmail.com>
2025-01-24 09:31:28 +0100

    kabot

--- Assets/--Scripts--/Kabot/KabotMovement.cs
+++ Assets/--Scripts--/Kabot/KabotMovement.cs
@@ -7,11 +7,21 @@ public class KabotMovement : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float rayLength = 1.5f;
+    [SerializeField] private GameObject player;
 
     [HideInInspector] public float Speed;
 
     private NavMeshAgent agent;
     private Quaternion lastRotation;
+
+    public enum KabotState
+    {
+        FollowPlayer,
+        GuidePlayer,
+        Sit
+    }
+
+    public KabotState CurrentState;
 
     void Start()
     {
@@ -20,29 +30,38 @@ public class KabotMovement : MonoBehaviour
 
     void Update()
     {
-        agent.destination = target.position;
-        Speed = agent.velocity.magnitude;
-
-        if (Speed > .01f)
+        if (CurrentState == KabotState.FollowPlayer)
         {
-            Ray ray = new Ray(transform.position, Vector3.down);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, rayLength, groundLayer))
+            target.position = player.transform.position;
+
+            agent.destination = target.position;
+            Speed = agent.velocity.magnitude;
+
+            if (Speed > .01f)
             {
-                Vector3 groundNormal = hitInfo.normal;
-                Vector3 moveDirection = agent.velocity.normalized;
+                Ray ray = new Ray(transform.position, Vector3.down);
+                if (Physics.Raycast(ray, out RaycastHit hitInfo, rayLength, groundLayer))
+                {
+                    Vector3 groundNormal = hitInfo.normal;
+                    Vector3 moveDirection = agent.velocity.normalized;
 
-                Vector3 right = Vector3.Cross(Vector3.up, moveDirection).normalized;
-                Vector3 alignedForward = Vector3.Cross(groundNormal, right).normalized;
+                    Vector3 right = Vector3.Cross(Vector3.up, moveDirection).normalized;
+                    Vector3 alignedForward = Vector3.Cross(groundNormal, right).normalized;
 
-                Quaternion targetRotation = Quaternion.LookRotation(alignedForward, groundNormal);
-                transform.rotation = targetRotation;
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
-                lastRotation = transform.rotation;
+                    Quaternion targetRotation = Quaternion.LookRotation(alignedForward, groundNormal);
+                    transform.rotation = targetRotation;
+                    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+                    lastRotation = transform.rotation;
+                }
+            }
+            else
+            {
+                transform.rotation = lastRotation;
             }
         }
         else
         {
-            transform.rotation = lastRotation;
+            print("false");
         }
     }
 }
