a10a8e357fc854d004637128aba2cdfec1932edf
1Coquo <1coquo@gmail.com>
2025-02-02 15:24:23 +0100

    Smooth switch state

--- Assets/--GP--/Scripts/Kabot/KabotMovement.cs
+++ Assets/--GP--/Scripts/Kabot/KabotMovement.cs
@@ -1,6 +1,7 @@
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Playables;
 
 public class KabotMovement : MonoBehaviour
 {
@@ -32,46 +33,74 @@ public class KabotMovement : MonoBehaviour
 
     void Update()
     {
-        if (CurrentState == KabotState.FollowPlayer)
+        MoveSmoothTarget();
+
+        CheckStates();
+
+        SetOrientation();
+    }
+
+    private void MoveSmoothTarget()
+    {
+        if (Vector3.Distance(smoothTarget.transform.position, target.position) > 0.1f)
         {
-            target.position = player.transform.position;
-
-            if (Vector3.Distance(smoothTarget.transform.position, target.position) > 2f)
-            {
-                Vector3 direction = (target.position - smoothTarget.transform.position).normalized;
-
-                smoothTarget.transform.position += direction * SmoothSpeed * Time.deltaTime;
-            }
-
             agent.destination = smoothTarget.position;
-            Speed = agent.velocity.magnitude;
+        }
 
-            if (Speed > .01f)
-            {
-                Ray ray = new Ray(transform.position, Vector3.down);
-                if (Physics.Raycast(ray, out RaycastHit hitInfo, rayLength, groundLayer))
+        Speed = agent.velocity.magnitude;
+    }
+
+    private void CheckStates()
+    {
+        switch (CurrentState)
+        {
+            case KabotState.FollowPlayer:
+                target.position = player.transform.position;
+
+                if (Vector3.Distance(smoothTarget.transform.position, target.position) > 2f)
                 {
-                    Vector3 groundNormal = hitInfo.normal;
-                    Vector3 moveDirection = agent.velocity.normalized;
-
-                    Vector3 right = Vector3.Cross(Vector3.up, moveDirection).normalized;
-                    Vector3 alignedForward = Vector3.Cross(groundNormal, right).normalized;
-
-                    Quaternion targetRotation = Quaternion.LookRotation(alignedForward, groundNormal);
-                    transform.rotation = targetRotation;
-                    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
-                    lastRotation = transform.rotation;
+                    MoveSmoothTargetToTarget();
                 }
-            }
-            else
+                break;
+
+            case KabotState.GuidePlayer:
+                MoveSmoothTargetToTarget();
+                break;
+
+            default:
+                Debug.LogWarning("État inconnu !");
+                break;
+        }
+    }
+
+    private void MoveSmoothTargetToTarget()
+    {
+        Vector3 direction = (target.position - smoothTarget.transform.position).normalized;
+        smoothTarget.transform.position += direction * SmoothSpeed * Time.deltaTime;
+    }
+
+    private void SetOrientation()
+    {
+        if (Speed > .01f)
+        {
+            Ray ray = new Ray(transform.position, Vector3.down);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, rayLength, groundLayer))
             {
-                transform.rotation = lastRotation;
+                Vector3 groundNormal = hitInfo.normal;
+                Vector3 moveDirection = agent.velocity.normalized;
+
+                Vector3 right = Vector3.Cross(Vector3.up, moveDirection).normalized;
+                Vector3 alignedForward = Vector3.Cross(groundNormal, right).normalized;
+
+                Quaternion targetRotation = Quaternion.LookRotation(alignedForward, groundNormal);
+                transform.rotation = targetRotation;
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+                lastRotation = transform.rotation;
             }
         }
         else
         {
-            Vector3 direction = (target.position - smoothTarget.transform.position).normalized;
-            smoothTarget.transform.position += direction * SmoothSpeed * Time.deltaTime;
+            transform.rotation = lastRotation;
         }
     }
 }
