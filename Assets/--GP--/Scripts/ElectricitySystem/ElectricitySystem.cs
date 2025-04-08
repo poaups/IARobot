afb26451acb9b763005b8200820ca885d868d959
1Coquo <1coquo@gmail.com>
2025-04-08 21:56:06 +0200

    AddMat

--- Assets/--GP--/Scripts/ElectricitySystem/ElectricitySystem.cs
+++ Assets/--GP--/Scripts/ElectricitySystem/ElectricitySystem.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class ElectricitySystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameObject Spline;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void 
 }
